751b0f0d5323e357a1e66b6e131b62a38b444c15
Louis Heimel <louis.heimel@mines.sdsmt.edu>
2015-08-19 16:46:57 -0500

    first draft of Zipline: Build a Random Quote Machine

--- ziplinequotes/quotes.js
+++ ziplinequotes/quotes.js
@@ -7,15 +7,41 @@ var quotes = [
   ["Marshall McLuhan","The medium is the message."],
   ["John Boyd","You gotta challenge all assumptions. If you don't, what is doctrine on day one becomes dogma forever after."]
 ]
+function clean(quote) {
+  var re = /;/gi
+  var newstr = quote.replace(re,"%3B");
+  return newstr;
+}
+
 
 
 
 $(document).ready(function() {
+  var index = Math.floor(Math.random()*quotes.length);
+  var word = '<h2>' + quotes[index][1] + '</h2>'+'<h3>'+quotes[index][0]+'</h3>';
+  $('#Content').append(word);
   $('#target').on('click', function () {
     $('#Content').empty();
-    var index = Math.floor(Math.random()*quotes.length);
-    var word = '<h1>' + quotes[index][1] + '</h1>'+'<h2>'+quotes[index][0]+'</h2>';
+    index = Math.floor(Math.random()*quotes.length);
+    word = '<h2>' + quotes[index][1] + '</h2>'+'<h3>'+quotes[index][0]+'</h3>';
 
     $('#Content').append(word);
+  });
+  $('#tweet').click(function(event) {
+    this.href = "http://twitter.com/share?" + "text=" + clean(quotes[index][1]) + '   - ' + quotes[index][0];
+    var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left;
+
+    window.open(url, 'twitter', opts);
+
+    return false;
   });
 });
